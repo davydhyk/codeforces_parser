d800be87eab0c265994dd1e2f88ddf9849d95f0d
davydhyk <davydhyk@gmail.com>
2018-11-22 01:09:53 +0200

    v1.0.1

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const cheerio = require('cheerio'),
-      http = require('http'),
-      https = require('https'),
-      fs = require('fs'),
-      rimraf = require('rimraf'),
-      Entities = require('html-entities').XmlEntities,
-      entities = new Entities(),
-      pretty = require('pretty');
+  http = require('http'),
+  https = require('https'),
+  fs = require('fs'),
+  rimraf = require('rimraf'),
+  Entities = require('html-entities').XmlEntities,
+  entities = new Entities(),
+  pretty = require('pretty');
 
 const pathToProblems = './problems',
-      startFolderName = 'A',
-      pathToTests = '/tests',
-      pathToLinks = './links.txt',
-      statementLinkSelector = '#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr:nth-child(2) > td:nth-child(3) > a',
-      statementSelector = '#pageContent > div.problemindexholder > div > div',
-      statementFileName = '/statement.html';
+  startFolderName = 'A',
+  pathToTests = '/tests',
+  pathToLinks = './links.txt',
+  statementLinkSelector = '#pageContent > div.datatable > div:nth-child(6) > table > tbody > tr:nth-child(2) > td:nth-child(3) > a',
+  statementSelector = '#pageContent > div.problemindexholder > div > div',
+  statementFileName = '/statement.html';
 
 const format = {
   task: '.dat',
@@ -40,24 +40,30 @@ function getTests(link) {
   if (link.indexOf('https') != -1) var query = https;
   else var query = http;
   return new Promise((resolve, reject) => {
-    query.get(link, function (resp) {
+    query.get(link, function(resp) {
       var tsk = [],
-          ans = [],
-          data = "";
-      resp.on('data', function (d) {data += d;});
-      resp.on('end', function () {
+        ans = [],
+        data = "";
+      resp.on('data', function(d) {
+        data += d;
+      });
+      resp.on('end', function() {
         var $ = cheerio.load(data);
-        $('.file.input-view').each(function (i, val) {
+        $('.file.input-view').each(function(i, val) {
           var input = $(val).find('.text pre').text(),
-              output = $(val).next().find('.text pre').text(); 
+            output = $(val).next().find('.text pre').text();
           if (input.indexOf('...') == -1 && output.indexOf('...') == -1) {
             tsk.push(input);
             ans.push(output);
           }
         });
-        resolve({task: tsk, answer: ans, link: $(statementLinkSelector).attr('href')});
+        resolve({
+          task: tsk,
+          answer: ans,
+          link: $(statementLinkSelector).attr('href')
+        });
       });
-      resp.on('error', function (err) {
+      resp.on('error', function(err) {
         reject(err);
       })
     });
@@ -68,14 +74,16 @@ function getStatement(link) {
   if (link.indexOf('https') != -1) var query = https;
   else var query = http;
   return new Promise((resolve, reject) => {
-    query.get(link, function (resp) {
+    query.get(link, function(resp) {
       var data = "";
-      resp.on('data', function (d) {data += d;});
-      resp.on('end', function () {
+      resp.on('data', function(d) {
+        data += d;
+      });
+      resp.on('end', function() {
         var $ = cheerio.load(data);
         resolve(pretty(entities.decode($(statementSelector).html())));
       });
-      resp.on('error', function (err) {
+      resp.on('error', function(err) {
         reject(err);
       })
     });
@@ -84,9 +92,9 @@ function getStatement(link) {
 
 function saveTests(path, tsk, ans) {
   return new Promise((resolve, reject) => {
-    for(var i=0;i<tsk.length;i++) {
+    for (var i = 0; i < tsk.length; i++) {
       var j = i + 1;
-          num = '/' + parseInt(j / 100) + parseInt(j % 100 / 10) + parseInt(j % 10);
+      num = '/' + parseInt(j / 100) + parseInt(j % 100 / 10) + parseInt(j % 10);
       fs.writeFile(path + num + format.task, tsk[i], () => {});
       fs.writeFile(path + num + format.answer, ans[i], () => {});
     }
@@ -97,15 +105,15 @@ function saveTests(path, tsk, ans) {
 console.log(links.length);
 
 async function start() {
-  for(var _i = 0;_i<links.length;_i++) {
+  for (var _i = 0; _i < links.length; _i++) {
     console.log(String.fromCharCode(startFolderName.charCodeAt(0) + _i));
     try {
       var result = await getTests(links[_i]);
       var statementLink = links[_i].split('/problemset')[0] + result.link + '?lang=ru';
       var tsk = result.task,
-          ans = result.answer,
-          name = '/' + String.fromCharCode(startFolderName.charCodeAt(0) + _i),
-          tmpPath = pathToProblems + name;
+        ans = result.answer,
+        name = '/' + String.fromCharCode(startFolderName.charCodeAt(0) + _i),
+        tmpPath = pathToProblems + name;
       fs.mkdirSync(tmpPath);
       fs.writeFileSync(tmpPath + statementFileName, await getStatement(statementLink));
       tmpPath += pathToTests;
